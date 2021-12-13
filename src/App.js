736ce2ee84cc8f81715b45d61a2fd540adbc47e5
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-12 21:08:38 -0500

    add state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import './App.css';
 
 function App() {
+
+const { register, handleSubmit, errors } = useForm();
+const [userInfo, setUserInfo] = useState();
+
+const onSubmit = (data) => {
+  setUserInfo(data);
+  console.log(data);
+}
+
   return (
     <div className="container">
-      <form className="form"> 
+      <prev>{JSON.stringify(userInfo, undefined,2)}</prev>
+      <form onSubmit={handleSubmit(onSubmit)} className="form"> 
         <h1>Registration Form</h1>
         <div className="ui divider"></div>
           <div className="ui form">
                <div className="field">
                  <label>Username</label>
-                 <input type="text" name="username" placeholder="Username" />
+                 <input type="text" name="username" placeholder="Username" ref={register} />
                </div>  
                <div className="field">
                  <label>Email</label>
-                 <input type="email" name="email" placeholder="Email" />
+                 <input type="email" name="email" placeholder="Email" ref={register} />
                </div>  
                <div className="field">
                  <label>Password</label>
-                 <input type="password" name="password" placeholder="Password" />
+                 <input type="password" name="password" placeholder="Password" ref={register} />
                </div>  
                <button className="fluid ui button blue">Submit</button>
           </div>
