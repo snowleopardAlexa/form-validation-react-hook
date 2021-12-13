2d21249fbff081d305277abd8b1e3c502cb37a2e
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-12 20:53:44 -0500

    add form ui

--- src/App.js
+++ src/App.js
@@ -3,8 +3,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="container">
+      <form>
+        <h1>Registration Form</h1>
+        <div className="ui divider">
+          <div className="ui form">
+               <div className="field">
+                 <label>Username</label>
+                 <input type="text" name="username" placeholder="Username" />
+               </div>  
+               <div className="field">
+                 <label>Username</label>
+                 <input type="email" name="email" placeholder="Email" />
+               </div>  
+               <div className="field">
+                 <label>Password</label>
+                 <input type="password" name="password" placeholder="Password" />
+               </div>  
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
